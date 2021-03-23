983128a10425fb61feb2850b24ea8f0e80d7e957
Eduardo Mora <32511969+Masetto97@users.noreply.github.com>
2021-03-23 16:35:20 +0100

    Update Servidor-File.py

--- Servidor-File.py
+++ Servidor-File.py
@@ -1,48 +1,54 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#      server.py
-#
-#      Copyright 2014 Recursos Python - www.recursospython.com
-#
-#
-import socket
-import os
 
-def main():
-    s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port = 5000 # Puerto de comunicacion
-    # Escuchar peticiones en el puerto 6030.
-    s.bind(('',port)) # IP y Puerto de conexion en una Tupla
-    s.listen(1)
-    
-    conn, addr = s.accept()
-    f = open("recibido.txt", "wb")
-    
-    while True:
-        try:
-            # Recibir datos del cliente.
-            input_data = conn.recv(1024)
-        except error:
-            print("Error de lectura.")
-            break
-        else:
-            if input_data:
-                # Compatibilidad con Python 3.
-                if isinstance(input_data, bytes):
-                    end = input_data[0] == 1
-                else:
-                    end = input_data == chr(1)
-                if not end:
-                    # Almacenar datos.
-                    f.write(input_data)
-                else:
+# Envio archivos: servidor
+# 11Sep
+
+import socket
+
+# Creamos una lista con los datos del la conexión
+CONEXION = ('', 5000)
+
+servidor = socket.socket()
+
+# Ponemos el servidor a la escucha
+servidor.bind(CONEXION)
+servidor.listen(5)
+print "Escuchando {0} en {1}".format(*CONEXION)
+# Aceptamos conexiones
+sck, addr = servidor.accept()
+print "Conectado a: {0}:{1}".format(*addr)
+while True:
+    # Recibimos la longitud que envia el cliente
+    recibido = sck.recv(1024).strip()
+    if recibido:
+        print "Recibido:", recibido
+    # Verificamos que lo que recibimos sea un número
+    # en caso que así sea, enviamos el mensaje "OK"
+    # al cliente indicandole que estamos listos
+    # para recibir el archivo
+    if recibido.isdigit():
+        sck.send("OK")
+       
+        # Inicializamos el contador que
+        # guardara la cantidad de bytes recibidos
+        buffer = 0
+        # Abrimos el archivo en modo escritura binaria
+        with open("Archivorecibido.txt", "wb") as archivo:
+            # Nos preparamos para recibir el archivo
+            # con la longitud específica
+            while (buffer <= int(recibido)):
+                data = sck.recv(1)
+                if not len(data):
+                    # Si no recibimos datos
+                    # salimos del bucle
                     break
-    
-    print("El archivo se ha recibido correctamente.")
-    f.close()
-    print("Mostrando los archivos actuales del directorio:")   
-    os.system('ls')
-     
-if __name__ == "__main__":
-    main()
+                # Escribimos cada byte en el archivo
+                # y aumentamos en uno el buffer
+                archivo.write(data)
+                buffer += 1
+           
+            if buffer == int(recibido):
+                print "Archivo descargado con éxito"
+            else:
+                print "Ocurrió un error/Archivo incompleto"
+        break
