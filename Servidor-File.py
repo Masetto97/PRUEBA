20469a7c765351e277e8dd5a0b42c77dc933e2cc
Eduardo Mora <32511969+Masetto97@users.noreply.github.com>
2021-04-11 12:27:07 +0200

    Update Servidor-File.py

--- Servidor-File.py
+++ Servidor-File.py
@@ -1,16 +1,21 @@
 import socket, pickle
-
-print "Server is Listening....."
-HOST = 'localhost'
-PORT = 5000
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', PORT))
+s = socket.socket()
+s.bind(("localhost", 50001))
 s.listen(1)
-conn, addr = s.accept()
-print 'Connected by', addr
-
-data = conn.recv(4096)
-data_variable = pickle.loads(data)
-conn.close()
-print data_variable
-print 'Data received from client'
+c,a = s.accept()
+filetodown = open("./recibido.txt", "wb")
+while True:
+   print("Receiving....")
+   aux = c.recv(1024)
+   if aux[-3:] == 'fin':
+        aux1 = aux[:-3]
+        data = pickle.loads(aux1)
+        filetodown.write(data)
+        break
+   data = pickle.loads(aux)
+   filetodown.write(data)
+filetodown.close()
+c.send("Thank you for connecting.")
+c.shutdown(2)
+c.close()
+s.close()
