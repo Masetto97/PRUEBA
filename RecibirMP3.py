e5933b997f5383ed9069991a434ded5c29742d73
Eduardo Mora <32511969+Masetto97@users.noreply.github.com>
2021-04-13 11:16:08 +0200

    Update RecibirMP3.py

--- RecibirMP3.py
+++ RecibirMP3.py
@@ -18,7 +18,7 @@ while True:
 filetodown.close()
 c.send("Thank you for connecting.")
 os.system('ls')
-os.system('cp salidaprueba.mid ../../app/Symbolic-Melody-Identification/')
+os.system('cp salidaprueba.mid ../../app/Symbolic-Melody-Identification/salidaprueba.mid')
 os.system('conda run -n IA ../../app1/Symbolic-Melody-Identification/terminal_client.py --model model.pkl --extract ../../app1/Symbolic-Melody-Identification/prueba2.mid edusalidaprueba1.mid')
 os.system('ls')
 c.shutdown(2)
